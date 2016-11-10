ca07dcf106229654cd664a053deb3826bfff9db0
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-11-10 15:10:56 -0500

    Migrated user, restaurant, res.

--- db/migrate/20161110193526_create_reservations.rb
+++ db/migrate/20161110193526_create_reservations.rb
@@ -2,11 +2,11 @@ class CreateReservations < ActiveRecord::Migration[5.0]
   def change
     create_table :reservations do |t|
       t.integer :user_id
-      t.integer :restaurant
+      t.integer :restaurant_id
       t.date    :date
       t.integer :time
-
-
+      t.integer :seats
+      
       t.timestamps
     end
   end
