64f8628fba9e9d5aeb67972672f6685e5dc8e3c5
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-11-10 16:17:11 -0500

    added controllers: restuarant, user, homepage, reservations added links to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  root 'home#index'
+
+  resources :reservations, only: %i(new index edit destroy show)
+
+  resources :users, only: %i(new index edit destroy show)
+
+  resources :restaurants, only: %i(new index edit destroy show)
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
