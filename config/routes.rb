42f8c3f2b2a5e9212390feb1a146181186196e6a
Eric Hartman <imerichartman@gmail.com>
2016-11-11 16:32:26 -0500

    fix reservations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  get "/unauthorized" => "pages#unauthorized"
+
 
   resources :users
+
+  resources :log_in, only: [:index, :create, :destroy]
+
 
   resources :restaurants do
     resources :reviews, only: [:show, :create, :destroy]
     resources :reservations
+
   end
-  resources :log_in, only: [:index, :create, :destroy]
-  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
