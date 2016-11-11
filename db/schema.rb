faa93d9531c7ff35aa696c4e9369a8f7b2242992
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-11-11 14:02:35 -0500

    Added models for review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110193526) do
+ActiveRecord::Schema.define(version: 20161111184938) do
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "score"
+    t.string   "default"
+    t.integer  "0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
