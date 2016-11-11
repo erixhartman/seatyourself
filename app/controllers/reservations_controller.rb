39c4065b4923c574cd01d56bf46592d11828b032
jgphilpott <jacob.gary.philpott@gmail.com>
2016-11-11 16:14:12 -0500

    Nessted Reservations under Restraunts

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,15 +1,41 @@
 class ReservationsController < ApplicationController
+  before_action :load_restaurant
+
   def index
+    @reservations = Reservation.all
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
   end
 
   def new
     @reservation = Reservation.new
   end
 
+  def create
+    @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      redirect_to root_url, notice: "Reservation made successfully!"
+    else
+      render 'restaurants/show'
+    end
+  end
+
   def edit
     @reservation = Reservation.find(params[:id])
   end
+
+  private
+
+  def reservation_params
+   params.require(:reservation).permit(:seats, :date, :time, :restaurant_id)
+  end
+
+ def load_restaurant
+   @restaurant = Restaurant.find(params[:restaurant_id])
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :reservations
-
   resources :users
 
   resources :restaurants do
     resources :reviews, only: [:show, :create, :destroy]
+    resources :reservations
   end
   resources :log_in, only: [:index, :create, :destroy]
 
