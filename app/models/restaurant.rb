e587b2266d87919149e8f1a45fc8686d3504a433
Eric Hartman <imerichartman@gmail.com>
2016-11-12 12:45:57 -0500

    Add validations to add restaurant form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews
   belongs_to :owner, class_name: "User"
   has_many :pictures
+
+  validates :name, :location, :cuisine, :open_time, :close_time, :price, presence: true
+  validates :max_seats, numericality: {only_integer: true}
 end
