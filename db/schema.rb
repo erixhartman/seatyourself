620b7169589f5b0c17fd7c49b306d4f44b9f119b
Eric Hartman <imerichartman@gmail.com>
2016-11-12 12:40:59 -0500

    Add price column to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111234607) do
+ActiveRecord::Schema.define(version: 20161112173535) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "url"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20161111234607) do
     t.integer  "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "price"
   end
 
   create_table "reviews", force: :cascade do |t|
