a7144d6aaa8ab52ecc0045d7f4abaa6b391edb43
jgphilpott <jacob.gary.philpott@gmail.com>
2016-11-13 18:45:47 -0500

    New Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,104 +5,592 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 Restaurant.create(
-name: "Burger's Priest",
+name: "Pizza Pizza",
+max_seats: 25,
+location: "toronto",
+cuisine: "pizza",
+open_time: "2000-01-01 07:30:00",
+close_time: "2000-01-01 19:30:00",
+score: 1,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Babu",
 max_seats: 50,
 location: "toronto",
-cuisine: "burgers",
-open_time: 11,
-close_time: 23,
+cuisine: "indian",
+open_time: "2000-01-01 08:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 2,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Beef Easy",
+max_seats: 100,
+location: "toronto",
+cuisine: "steak house",
+open_time: "2000-01-01 13:30:00",
+close_time: "2000-01-01 23:30:00",
+score: 3,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Bakehouse",
+max_seats: 30,
+location: "toronto",
+cuisine: "café",
+open_time: "2000-01-01 06:00:00",
+close_time: "2000-01-01 19:30:00",
+score: 4,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
 price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Boston Pizza",
+max_seats: 80,
+location: "pizza",
+cuisine: "pizza",
+open_time: "2000-01-01 14:30:00",
+close_time: "2000-01-01 22:30:00",
+score: 5,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "India 360",
+max_seats: 55,
+location: "toronto",
+cuisine: "indian",
+open_time: "2000-01-01 07:30:00",
+close_time: "2000-01-01 19:30:00",
+score: 1,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Pizza Nova",
+max_seats: 35,
+location: "toronto",
+cuisine: "pizza",
+open_time: "2000-01-01 15:30:00",
+close_time: "2000-01-01 21:30:00",
+score: 2,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "The Keg",
+max_seats: 110,
+location: "vancouver",
+cuisine: "steak house",
+open_time: "2000-01-01 13:30:00",
+close_time: "2000-01-01 23:59:00",
+score: 3,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Harvey’s",
+max_seats: 60,
+location: "vancouver",
+cuisine: "burgers",
+open_time: "2000-01-01 012:00:00",
+close_time: "2000-01-01 20:30:00",
+score: 4,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Fickle Pickle",
+max_seats: 95,
+location: "vancouver",
+cuisine: "dinner",
+open_time: "2000-01-01 07:00:00",
+close_time: "2000-01-01 19:30:00",
+score: 5,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Frankie Tomato’s",
+max_seats: 235,
+location: "vancouver",
+cuisine: "buffet",
+open_time: "2000-01-01 012:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 1,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Fire and Ice",
+max_seats: 65,
+location: "vancouver",
+cuisine: "steak house",
+open_time: "2000-01-01 014:30:00",
+close_time: "2000-01-01 22:30:00",
+score: 2,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Sushi Hut",
+max_seats: 15,
+location: "vancouver",
+cuisine: "sushi",
+open_time: "2000-01-01 07:00:00",
+close_time: "2000-01-01 19:00:00",
+score: 3,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Play",
+max_seats: 35,
+location: "vancouver",
+cuisine: "tapas",
+open_time: "2000-01-01 12:00:00",
+close_time: "2000-01-01 23:00:00",
+score: 4,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Tim Horton’s",
+max_seats: 35,
+location: "edmonton",
+cuisine: "café",
+open_time: "2000-01-01 00:01:00",
+close_time: "2000-01-01 23:59:00",
+score: 5,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Wendy’s",
+max_seats: 45,
+location: "edmonton",
+cuisine: "burgers",
+open_time: "2000-01-01 07:00:00",
+close_time: "2000-01-01 20:00:00",
+score: 1,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Subway",
+max_seats: 10,
+location: "edmonton",
+cuisine: "sub",
+open_time: "2000-01-01 07:00:00",
+close_time: "2000-01-01 19:00:00",
+score: 2,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Mr. Sub",
+max_seats: 15,
+location: "edmonton",
+cuisine: "sub",
+open_time: "2000-01-01 07:30:00",
+close_time: "2000-01-01 19:30:00",
+score: 3,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Chipotle Mexican Grill",
+max_seats: 40,
+location: "edmonton",
+cuisine: "mexican",
+open_time: "2000-01-01 012:00:00",
+close_time: "2000-01-01 22:30:00",
+score: 4,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Starbucks",
+max_seats: 30,
+location: "edmonton",
+cuisine: "café",
+open_time: "2000-01-01 06:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 5,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Swiss Chalet",
+max_seats: 75,
+location: "edmonton",
+cuisine: "dinner",
+open_time: "2000-01-01 09:30:00",
+close_time: "2000-01-01 22:30:00",
+score: 1,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Hero Burger",
+max_seats: 25,
+location: "montreal",
+cuisine: "burgers",
+open_time: "2000-01-01 10:30:00",
+close_time: "2000-01-01 19:00:00",
+score: 2,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Moxie’s",
+max_seats: 85,
+location: "montreal",
+cuisine: "dinner",
+open_time: "2000-01-01 11:30:00",
+close_time: "2000-01-01 22:30:00",
+score: 3,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Jackasters",
+max_seats: 90,
+location: "montreal",
+cuisine: "dinner",
+open_time: "2000-01-01 07:30:00",
+close_time: "2000-01-01 22:30:00",
+score: 4,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Milestones",
+max_seats: 130,
+location: "montreal",
+cuisine: "dinner",
+open_time: "2000-01-01 14:30:00",
+close_time: "2000-01-01 23:30:00",
+score: 5,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 4,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "CN Tower 360",
+max_seats: 150,
+location: "montreal",
+cuisine: "fine dinning",
+open_time: "2000-01-01 13:00:00",
+close_time: "2000-01-01 23:59:00",
+score: 5,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Mandarin",
+max_seats: 200,
+location: "montreal",
+cuisine: "buffet",
+open_time: "2000-01-01 14:30:00",
+close_time: "2000-01-01 23:30:00",
+score: 1,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Mr. Greek",
+max_seats: 55,
+location: "montreal",
+cuisine: "greek",
+open_time: "2000-01-01 14:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 2,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
+)
+
+Restaurant.create(
+name: "Burger's Priest",
+max_seats: 60,
+location: "ottawa",
+cuisine: "burgers",
+open_time: "2000-01-01 07:00:00",
+close_time: "2000-01-01 20:30:00",
+score: 3,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 1,
+description: "N/A",
 )
 
 Restaurant.create(
 name: "Holy Chuck",
-max_seats: 40,
-location: "vancouver",
-cuisine: "burgers",
-open_time: 11,
-close_time: 23,
-price: 2,
+max_seats: 80,
+location: "ottawa",
+cuisine: "dinner",
+open_time: "2000-01-01 09:30:00",
+close_time: "2000-01-01 22:30:00",
+score: 4,
+owner_id: 2,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 3,
+description: "N/A",
 )
 
 Restaurant.create(
 name: "Pizza Gigi",
-max_seats: 29,
-location: "calgary",
+max_seats: 35,
+location: "ottawa",
 cuisine: "pizza",
-open_time: 16,
-close_time: 23,
-price: 3,
+open_time: "2000-01-01 12:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 5,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
+price: 2,
+description: "N/A",
 )
 
 Restaurant.create(
 name: "Bitondos",
-max_seats: 16,
-location: "edmonton",
-cuisine: "pizza",
-open_time: 10,
-close_time: 20,
+max_seats: 50,
+location: "ottawa",
+cuisine: "dinner",
+open_time: "2000-01-01 07:30:00",
+close_time: "2000-01-01 19:30:00",
+score: 1,
+owner_id: 4,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
 price: 4,
+description: "N/A",
 )
 
 Restaurant.create(
 name: "North of Brooklyn",
-max_seats: 80,
-location: "Montreal",
-cuisine: "pizza",
-open_time: 11,
-close_time: 23,
+max_seats: 55,
+location: "ottawa",
+cuisine: "dinner",
+open_time: "2000-01-01 11:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 2,
+owner_id: 1,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
 price: 1,
+description: "N/A",
 )
 
 Restaurant.create(
 name: "New Sky",
-max_seats: 100,
-location: "toronto",
-cuisine: "chinese",
-open_time: 5,
-close_time: 23,
+max_seats: 60,
+location: "ottawa",
+cuisine: "dinner",
+open_time: "2000-01-01 09:30:00",
+close_time: "2000-01-01 20:30:00",
+score: 3,
+owner_id: 2,
+created_at: "2016-11-13 23:59:58",
+updated_at: "2016-11-13 23:59:59",
 price: 2,
+description: "N/A",
 )
 
 Restaurant.create(
 name: "Rol San",
-max_seats: 200,
+max_seats: 40,
 location: "ottawa",
-cuisine: "chinese",
-open_time: 6,
-close_time: 22,
+cuisine: "dinner",
+open_time: "2000-01-01 09:00:00",
+close_time: "2000-01-01 20:30:00",
+score: 4,
+owner_id: 3,
+created_at: "2016-11-13 22:59:59",
+updated_at: "2016-11-13 23:59:59",
 price: 3,
+description: "N/A",
 )
 
 User.create(
-user_name: "erix",
-email: "e@h.com",
-phone: 1,
+user_name: "Eric",
+email: "eric@email.com",
+phone: 4166450086,
 password: "123",
 password_confirmation: "123",
 )
 
 User.create(
-user_name: "luke",
-email: "l@p.com",
-phone: 2,
+user_name: "Luke",
+email: "luke@email.com",
+phone: 2086459086,
 password: "123",
 password_confirmation: "123",
 )
 
 User.create(
-user_name: "jacob",
-email: "j@p.com",
-phone: 3,
+user_name: "Jacob",
+email: "jacob@email.com",
+phone: 9056420086,
 password: "123",
 password_confirmation: "123"
 )
 
 User.create(
-user_name: "evan",
-email: "e@s.com",
-phone: 4,
+user_name: "Evan",
+email: "evan@email.com",
+phone: 6476424286,
+password: "123",
+password_confirmation: "123"
+)
+
+User.create(
+user_name: "David",
+email: "david@email.com",
+phone: 4166450456,
+password: "123",
+password_confirmation: "123",
+)
+
+User.create(
+user_name: "Andrew",
+email: "andrew@email.com",
+phone: 2776459226,
+password: "123",
+password_confirmation: "123",
+)
+
+User.create(
+user_name: "Emily",
+email: "emily@email.com",
+phone: 9056270086,
+password: "123",
+password_confirmation: "123"
+)
+
+User.create(
+user_name: "Beth",
+email: "beth@email.com",
+phone: 6226444286,
 password: "123",
 password_confirmation: "123"
 )
