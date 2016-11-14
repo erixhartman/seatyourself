18f82c822995ad6256bd1ad8c395a7be21e226ee
Eric Hartman <imerichartman@gmail.com>
2016-11-14 16:00:29 -0500

    Cannot add more than max across all reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,20 +9,19 @@ class Reservation < ApplicationRecord
  def available
    if self.seats <= 0
      errors.add(:seats, "You must reserve at least one seat")
-   elsif self.restaurant.max_seats < self.seats
+   elsif available_capacity < self.seats
      errors.add(:seats, "There is not a reservation available for your party at this time")
    end
  end
 
 
- private
  def available_capacity
-   max_seats - reservations_at(time).sum(:seats)
+   restaurant.max_seats - restaurant.reservations.sum(:seats)
  end
-
- def reservations_at(time)
-   reservations.where(time: time.beginning_of_hour..time.end_of_hour)
- end
+ #
+ # def reservations_at(time)
+ #   self.reservations.where(time: time.beginning_of_hour..time.end_of_hour)
+ # end
 
 
 end
